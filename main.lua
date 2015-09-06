2f3085acee79dd600e262c60043f8e804c7916bc
Maarten Oosting <maarten.tutoworld@gmail.com>
2015-09-06 17:18:35 +0200

    Added fonts

--- main.lua
+++ main.lua
@@ -1,19 +1,26 @@
 function love.load()
     love.graphics.setBackgroundColor( 255,255,255 )
-end
- 
-function love.update(dt)
-     
-end
- 
-function love.draw()
-    w = love.graphics.getWidth() /2
-    h = love.graphics.getHeight() /2
-    love.graphics.translate( w, h );
 
-    love.graphics.setColor(0, 0, 0);
-    love.graphics.print("Hello World!",0,0)
-    love.graphics.circle("line", 0, 0, w/2, 128)
+    fonts = {}
+    fonts[16] = love.graphics.newFont("res/SF Intermosaic B.ttf", 16)
+    fonts[24] = love.graphics.newFont("res/SF Intermosaic.ttf", 24)
+    fonts[32] = love.graphics.newFont("res/SF Intermosaic.ttf", 32)
+    fonts[48] = love.graphics.newFont("res/SF Intermosaic.ttf", 48)
+    fonts[64] = love.graphics.newFont("res/SF Intermosaic.ttf", 64)
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.setFont(fonts[48])
+    w = love.graphics.getWidth() /2
+    h = 0;
+    --love.graphics.translate( w, h );
+
+    love.graphics.setColor(255, 0, 0);
+    love.graphics.print("Control The Chaos",0,0,0)
 end
 
 function love.keypressed(key)
